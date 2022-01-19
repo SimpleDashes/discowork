dbf55af2700ce11bc7fe18999f00efe406819a58
o-dasher <88356162+o-dasher@users.noreply.github.com>
2022-01-18 20:32:22 -0400

    Update index.ts

--- src/commands/decorators/index.ts
+++ src/commands/decorators/index.ts
@@ -1,10 +1,11 @@
 import type { ConstructorType } from "../../types";
 import type BaseCommandInterface from "../types/BaseCommandInterface";
 
-export function SetupCommand(options: { name: string; description: string }) {
-  return (
-    target: ConstructorType<[...unknown[]], BaseCommandInterface>
-  ): void => {
+export function CommandInformation(options: {
+  name: string;
+  description: string;
+}) {
+  return (target: ConstructorType<[], BaseCommandInterface>): void => {
     const { name, description } = options;
     target.prototype.name = name;
     target.prototype.description = description;
